d779a1d159650d1c5e248941a73f72955124a526
bytrustu <bytrustu@gmail.com>
2020-07-09 01:44:07 +0900

    동물 좌표이동 추가

--- src/Game.ts
+++ src/Game.ts
@@ -1,10 +1,14 @@
 import {Board, DeadZone} from "./Board";
+import {Player, PlayerType} from "./Player";
+import  "./Piece"
 
 export class Game {
     readonly board = new Board();
     readonly upperDeadZone = new DeadZone('upper');
     readonly lowerDeadZone = new DeadZone('lower');
 
+    readonly upperPlayer = new Player(PlayerType.UPPER)
+    readonly lowerPlayer = new Player(PlayerType.LOWER)
     constructor() {
         const boardContainer = document.querySelector('.board-container');
         boardContainer.firstChild.remove();
